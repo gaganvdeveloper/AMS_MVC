6380439570d701a4ef7d9a29e324e93b3bba0f7f
PavansaiPodichervu <podichervupavansai@gmail.com>
2023-10-27 12:20:46 +0530

    persistence.xml added

--- src/main/java/com/tyss/ams_mvc/config/MyConfiguration.java
+++ src/main/java/com/tyss/ams_mvc/config/MyConfiguration.java
@@ -2,15 +2,16 @@ package com.tyss.ams_mvc.config;
 
 import javax.persistence.EntityManager;
 import javax.persistence.Persistence;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 @ComponentScan(basePackages = "com.tyss.ams_mvc")
 public class MyConfiguration {
-	@Bean(name = "entityManager")
+	@Bean(value = "entityManager")
+	@Scope(value = "prototype")
 	public EntityManager getEntityManager() {
 		return Persistence.createEntityManagerFactory("dev").createEntityManager();
 	}
