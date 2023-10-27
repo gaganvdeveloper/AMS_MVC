15418590e83a783cc87c0ddb874d8050b9890de0
Shubhamkumardeveloper <shubhamkrsam1809@gmail.com>
2023-10-27 18:40:58 +0530

    Modifed User Model

--- src/main/java/com/tyss/ams_mvc/controller/UserController.java
+++ src/main/java/com/tyss/ams_mvc/controller/UserController.java
@@ -18,54 +18,64 @@ public class UserController {
 
 	@Autowired
 	private UserServiceImp userServiceImp;
+
 	@RequestMapping(value = "/userlogin", method = RequestMethod.GET)
 	public ModelAndView gotoSaveUser(ModelAndView mv) {
 		mv.setViewName("userlogin");
 		return mv;
 	}
-	
+
 	@RequestMapping(value = "/userloginvalidate")
 	public ModelAndView userVerification(HttpServletRequest req, ModelAndView mv) {
 		String email = req.getParameter("email");
 		String password = req.getParameter("password");
-		User user = (User)req;
-		if(user!= null && user.getUserRole() == UserRole.valueOf("TRAINER")) {
+		User user = (User) req;
+		if (user != null && user.getUserRole() == UserRole.valueOf("TRAINER")) {
 			mv.setViewName("trainerhome");
-			mv.addObject("user",user);
-			return mv;
-		}	
-		if(user!= null && user.getUserRole() == UserRole.valueOf("HR")) {
-			mv.setViewName("hrhome");
-			mv.addObject("user",user);
+			mv.addObject("user", user);
 			return mv;
 		}
-		if(user!= null && user.getUserRole() == UserRole.valueOf("ADMIN")) {
+		if (user != null && user.getUserRole() == UserRole.valueOf("HR")) {
+			mv.setViewName("hrhome");
+			mv.addObject("user", user);
+			return mv;
+		}
+		if (user != null && user.getUserRole() == UserRole.valueOf("ADMIN")) {
 			mv.setViewName("adminhome");
-			mv.addObject("user",user);
+			mv.addObject("user", user);
 			return mv;
 		}
 		mv.setViewName("login");
-		mv.addObject("user",user);
+		mv.addObject("user", user);
 		return mv;
 	}
-	
+
 	@RequestMapping(value = "/user", method = RequestMethod.GET)
-	public ModelAndView gotosaveUser(User user,ModelAndView mv) {
+	public ModelAndView gotosaveUser(User user, ModelAndView mv) {
 		mv.addObject(user);
 		mv.setViewName("userregister");
 		return mv;
 	}
-	
-	@RequestMapping(value = "/saveuser",method =RequestMethod.POST)
-	public ModelAndView saveUser(@ModelAttribute User user,ModelAndView mv) {
+
+	@RequestMapping(value = "/saveuser", method = RequestMethod.POST)
+	public ModelAndView saveUser(@ModelAttribute User user, ModelAndView mv) {
 		userServiceImp.saveUser(user);
-		mv.addObject("msg","User Created Successfully    User Name : "+user.getName());
+		mv.addObject("msg", "User Created Successfully    User Name : " + user.getName());
 		mv.setViewName("trainerhome");
 		return mv;
 	}
-	
-	
-	
-	
+
+	@RequestMapping(value = "/updatetrainer")
+	public ModelAndView updateUser(ModelAndView mv, HttpServletRequest req) {
+		User user = userServiceImp.findUserById(Integer.parseInt(req.getParameter("id")));
+		if (user != null) {
+			mv.addObject(user);
+			mv.setViewName("trainerupdate");
+			return mv;
+		}
+		mv.addObject("msg", "Invalid User Id Can't Update...");
+		mv.setViewName("trainerhome");
+		return mv;
+	}
 
 }
