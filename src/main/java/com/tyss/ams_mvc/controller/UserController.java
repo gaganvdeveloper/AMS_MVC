d3486f5b31487a8fd51da719510e3291ec41c331
Shubhamkumardeveloper <shubhamkrsam1809@gmail.com>
2023-10-28 09:49:18 +0530

    first commit

--- src/main/java/com/tyss/ams_mvc/controller/UserController.java
+++ src/main/java/com/tyss/ams_mvc/controller/UserController.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -60,8 +61,14 @@ public class UserController {
 		mv.setViewName("userregister");
 		return mv;
 	}
-
-
+	
+	@RequestMapping(value = "/saveuser")
+	public ModelAndView saveUser(@ModelAttribute User user, ModelAndView mv) {
+		userService.saveUser(user);
+		mv.addObject("msg","User Created Successfully..."+user.getName());
+		mv.setViewName("login");
+		return mv;
+	}
 
 	@RequestMapping(value = "/updatetrainer")
 	public ModelAndView updateUser(ModelAndView mv, HttpServletRequest req) {
